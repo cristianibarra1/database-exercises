323013925412923db53d82b3500aeffec0ef1358
cristianibarra1 <cristian.angel.ibarra@gmail.com>
2022-07-07 14:29:49 -0500

    join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -20,7 +20,7 @@ SELECT dept_name AS 'Department Name',CONCAT(e.first_name, ' ', e.last_name) AS 
 JOIN dept_manager dm ON d.dept_no = dm.dept_no 
 JOIN employees e ON dm.emp_no = e.emp_no 
 join salaries s on dm.emp_no = s.emp_no 
-WHERE dm.to_date > NOW();
+WHERE s.to_date >= NOW() AND dm.to_date >=NOW();
 --  Find the number of current employees in each department.
 --
 SELECT d.dept_no AS 'dept_no',d.dept_name AS 'dept_name',COUNT(*) AS 'num_employees'FROM departments AS d 
@@ -33,22 +33,28 @@ join dept_emp de on d.dept_no= de.dept_no
 join salaries s on de.emp_no=s.emp_no
 WHERE de.to_date > NOW() AND s.to_date > NOW()GROUP BY d.dept_name ORDER BY AVG(s.salary) DESC LIMIT 1;
 -- Who is the highest paid employee in the Marketing department?
+--
 SELECT e.first_name, e.last_name FROM departments d
 JOIN dept_emp de ON d.dept_no = de.dept_no
 JOIN employees e ON de.emp_no = e.emp_no
 JOIN salaries s ON e.emp_no = s.emp_no
 WHERE d.dept_name = 'Marketing' AND de.to_date > NOW()AND s.to_date > NOW()ORDER BY s.salary desc LIMIT 1;
 -- Which current department manager has the highest salary?
+--
 select e.first_name,e.last_name,s.salary,d.dept_name from departments d
 JOIN dept_manager de ON d.dept_no = de.dept_no
 JOIN employees e ON de.emp_no = e.emp_no
 JOIN salaries s ON e.emp_no = s.emp_no
 where e.first_name='Vishwani' and de.to_date > NOW()AND s.to_date > NOW()ORDER BY s.salary ;
 -- Determine the average salary for each department. Use all salary information and round your results.
+--
 SELECT dept_name AS 'Department Name',avg(s.salary) as salary from departments d 
 join dept_emp de on d.dept_no= de.dept_no
 join salaries s on de.emp_no=s.emp_no
 GROUP BY d.dept_name ORDER BY AVG(s.salary);
 --
 -- learning more 
-
+SELECT dept_name AS 'Department Name',CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' FROM departments d 
+JOIN dept_manager dm ON d.dept_no = dm.dept_no 
+JOIN employees e ON dm.emp_no = e.emp_no 
+where dm.to_date > NOW() and gender like '%f%';
